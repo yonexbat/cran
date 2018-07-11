f33fa36acea93be8f205638b38195b918f5bf75b
Claude Glauser <public@claude-glauser.ch>
2018-07-11 22:52:55 +0200

    Testcase für Ask-queston component

--- cranangularclient/src/app/ask-question/ask-question.component.spec.ts
+++ cranangularclient/src/app/ask-question/ask-question.component.spec.ts
@@ -1,21 +1,68 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input, DebugElement} from '@angular/core';
+
 import {FormsModule} from '@angular/forms';
 import { AskQuestionComponent } from './ask-question.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import {VoteComponent} from '../vote/vote.component';
+import { CRAN_SERVICE_TOKEN } from '../cran-data.servicetoken';
+import {NotificationService} from '../notification.service';
+import {SafeHtmlPipe} from '../save-html.pipe';
+import {ConfirmService} from '../confirm.service';
+import {LanguageService} from '../language.service';
 
+@Component({selector: 'app-vote', template: ''})
+class StubVoteComponent {
+  @Input() public votes;
+}
 
+@Component({selector: 'app-tags', template: ''})
+class StubTagsComponent {
+  @Input() public tagList;
+}
+
+@Component({selector: 'app-imagelist', template: ''})
+class StubAppImageListComponent {
+  @Input() public images;
+}
+
+@Component({selector: 'app-icon', template: ''})
+class StubIconComponent {
+  @Input() public icon;
+}
+
+@Component({selector: 'app-comments', template: ''})
+class CommentsComponent {
+}
+
+/*
+StubVoteComponent
+@Component({selector: 'app-icon ', template: ''})
+class StubIconComponent {
+  @Input() public icon;
+}*/
 
 describe('AskQuestionComponent', () => {
   let component: AskQuestionComponent;
   let fixture: ComponentFixture<AskQuestionComponent>;
 
   beforeEach(async(() => {
+    const cranDataService = jasmine.createSpyObj('CranDataService', ['vote']);
+    const notificationService = jasmine.createSpyObj('NotificationService', ['emitLoading', 'emitDone', 'emitError']);
+    const confirmationService = jasmine.createSpyObj('ConfirmService', ['some']);
+    const languageService = jasmine.createSpyObj('LanguageService', ['some']);
+
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, FormsModule],
-      declarations: [ AskQuestionComponent ],
-      providers: [VoteComponent]
+      declarations: [ AskQuestionComponent,  StubVoteComponent,
+        StubTagsComponent, SafeHtmlPipe, StubAppImageListComponent,
+        StubIconComponent, CommentsComponent],
+      providers: [
+        { provide: CRAN_SERVICE_TOKEN, useValue: cranDataService },
+        { provide: NotificationService, useValue: notificationService },
+        { provide: ConfirmService, useValue: confirmationService },
+        { provide: LanguageService, useValue: languageService }
+      ]
     })
     .compileComponents();
   }));
