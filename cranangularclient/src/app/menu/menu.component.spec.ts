064575b274b83885b5a1f33c2042999c6dfd9e4a
Claude Glauser <public@claude-glauser.ch>
2018-07-14 15:02:34 +0200

    Weitere Unit Tests

--- cranangularclient/src/app/menu/menu.component.spec.ts
+++ cranangularclient/src/app/menu/menu.component.spec.ts
@@ -1,7 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import {FormsModule} from '@angular/forms';
+import { Component, Input, DebugElement, TemplateRef} from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 
+import { CRAN_SERVICE_TOKEN } from '../cran-data.servicetoken';
+import {NotificationService} from '../notification.service';
+import {ConfirmService} from '../confirm.service';
+import {LanguageService} from '../language.service';
+import {PagedResult} from '../model/pagedresult';
 import { MenuComponent } from './menu.component';
+
+@Component({selector: 'app-user-info', template: ''})
+class StubUserInfoComponent {
+
+}
 
 describe('MenuComponent', () => {
 
@@ -10,9 +23,20 @@ describe('MenuComponent', () => {
   let fixture: ComponentFixture<MenuComponent>;
 
   beforeEach(async(() => {
+    const cranDataService = jasmine.createSpyObj('CranDataService', ['vote']);
+    const notificationService = jasmine.createSpyObj('NotificationService', ['emitLoading', 'emitDone', 'emitError']);
+    const confirmationService = jasmine.createSpyObj('ConfirmService', ['some']);
+
+
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [ MenuComponent ],
+      imports: [RouterTestingModule, FormsModule],
+      declarations: [ MenuComponent, StubUserInfoComponent ],
+      providers: [
+        LanguageService,
+        { provide: CRAN_SERVICE_TOKEN, useValue: cranDataService },
+        { provide: NotificationService, useValue: notificationService },
+        { provide: ConfirmService, useValue: confirmationService },
+      ],
     })
     .compileComponents();
   }));
--- cranangularclient/src/app/notification/notification.component.spec.ts
+++ cranangularclient/src/app/notification/notification.component.spec.ts
@@ -1,5 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import { Component, Input, DebugElement, TemplateRef} from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { CRAN_SERVICE_TOKEN } from '../cran-data.servicetoken';
+import {NotificationService} from '../notification.service';
+import {ConfirmService} from '../confirm.service';
+import {LanguageService} from '../language.service';
+import {PagedResult} from '../model/pagedresult';
 import { NotificationComponent } from './notification.component';
 
 describe('NotificationComponent', () => {
@@ -7,8 +15,17 @@ describe('NotificationComponent', () => {
   let fixture: ComponentFixture<NotificationComponent>;
 
   beforeEach(async(() => {
+    const cranDataService = jasmine.createSpyObj('CranDataService', ['vote']);
+    const confirmationService = jasmine.createSpyObj('ConfirmService', ['some']);
+
     TestBed.configureTestingModule({
-      declarations: [ NotificationComponent ]
+      imports: [RouterTestingModule, FormsModule],
+      declarations: [ NotificationComponent ],
+      providers: [
+        LanguageService, NotificationService,
+        { provide: CRAN_SERVICE_TOKEN, useValue: cranDataService },
+        { provide: ConfirmService, useValue: confirmationService },
+      ],
     })
     .compileComponents();
   }));
