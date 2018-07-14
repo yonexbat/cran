5b3397dd2e6e6904733c088ec60b62ad136b1ec6
Claude Glauser <public@claude-glauser.ch>
2018-07-14 17:15:18 +0200

    notification component umgesetzt

--- cranangularclient/src/app/notification/notification.component.spec.ts
+++ cranangularclient/src/app/notification/notification.component.spec.ts
@@ -1,14 +1,13 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick } from '@angular/core/testing';
 import {FormsModule} from '@angular/forms';
-import { Component, Input, DebugElement, TemplateRef} from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { CRAN_SERVICE_TOKEN } from '../cran-data.servicetoken';
 import {NotificationService} from '../notification.service';
 import {ConfirmService} from '../confirm.service';
 import {LanguageService} from '../language.service';
-import {PagedResult} from '../model/pagedresult';
 import { NotificationComponent } from './notification.component';
+import {NotificationEvent, NotificationType} from '../model/notificationEvent';
 
 describe('NotificationComponent', () => {
   let component: NotificationComponent;
@@ -39,4 +38,53 @@ describe('NotificationComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('emit an error', async(() => {
+    const notificationService: NotificationService = fixture.debugElement.injector.get(NotificationService);
+    notificationService.on().subscribe((res: NotificationEvent) => {
+      fixture.whenStable().then(() => {
+        fixture.detectChanges();
+
+        const loader = fixture.nativeElement.querySelector('.loader');
+        expect(loader).toBeFalsy('loading shoud not be visible');
+
+        const error = fixture.nativeElement.querySelector('.alert-danger');
+        expect(error).toBeTruthy('Errormessage shoud be showing');
+
+        const textcontent = fixture.nativeElement.textContent;
+        expect(textcontent).toContain('howdi');
+      });
+    });
+    notificationService.emitError('howdi');
+  }));
+
+  it('emit an error without when stable', async(() => {
+    const notificationService: NotificationService = fixture.debugElement.injector.get(NotificationService);
+    notificationService.on().subscribe((res: NotificationEvent) => {
+
+        fixture.detectChanges();
+
+        const loader = fixture.nativeElement.querySelector('.loader');
+        expect(loader).toBeFalsy('loading shoud not be visible');
+
+        const error = fixture.nativeElement.querySelector('.alert-danger');
+        expect(error).toBeTruthy('Errormessage shoud be showing');
+
+        const textcontent = fixture.nativeElement.textContent;
+        expect(textcontent).toContain('howdi');
+
+    });
+    notificationService.emitError('howdi');
+  }));
+
+  it('emit a loading notificaton', async(() => {
+    const notificationService = fixture.debugElement.injector.get(NotificationService);
+    notificationService.emitLoading();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+
+      const loader = fixture.nativeElement.querySelector('.loader');
+      expect(loader).toBeTruthy('loading shoud be visible');
+    });
+  }));
 });
