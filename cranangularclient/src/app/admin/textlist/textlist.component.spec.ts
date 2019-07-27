b7b7d39e94ee5fa21a3596ceea53dbff171af3d2
Claude Glauser <public@claude-glauser.ch>
2019-07-27 19:01:06 +0200

    Unit-Tests

--- cranangularclient/src/app/admin/textlist/textlist.component.spec.ts
+++ cranangularclient/src/app/admin/textlist/textlist.component.spec.ts
@@ -1,14 +1,54 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import { Component, Input, Output, EventEmitter, DebugElement, TemplateRef} from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { CRAN_SERVICE_TOKEN } from '../../cran-data.servicetoken';
+import {NotificationService} from '../../notification.service';
+import {ConfirmService} from '../../confirm.service';
+import {LanguageService} from '../../language.service';
+import {Tag} from '../../model/tag';
 import { TextlistComponent } from './textlist.component';
+import {TooltipDirective} from '../../uicomps/tooltip.directive';
+import {IconComponent} from '../../uicomps/icon/icon.component';
+import {PagedResult} from '../../model/pagedresult';
+import {UicompsModule} from '../../uicomps/uicomps.module';
+
+@Component({selector: 'app-pager', template: ''})
+class StubPagerComponent {
+  @Input()
+  public itemTemplate: TemplateRef<any>;
+  @Input()
+  public pagedResult: PagedResult<any>;
+  @Input()
+  public nodatafoundmessage  = 'Keine Daten gefunden.';
+}
 
 describe('TextlistComponent', () => {
   let component: TextlistComponent;
   let fixture: ComponentFixture<TextlistComponent>;
 
   beforeEach(async(() => {
+
+    const cranDataService = jasmine.createSpyObj('CranDataService', ['vote']);
+    const notificationService = jasmine.createSpyObj('NotificationService', ['emitLoading', 'emitDone', 'emitError']);
+    const confirmationService = jasmine.createSpyObj('ConfirmService', ['some']);
+
     TestBed.configureTestingModule({
-      declarations: [ TextlistComponent ]
+      imports: [
+        RouterTestingModule,
+        FormsModule,
+        UicompsModule,
+      ],
+      declarations: [
+        TextlistComponent,
+        StubPagerComponent ],
+      providers: [
+        LanguageService,
+        { provide: CRAN_SERVICE_TOKEN, useValue: cranDataService },
+        { provide: NotificationService, useValue: notificationService },
+        { provide: ConfirmService, useValue: confirmationService },
+      ],
     })
     .compileComponents();
   }));
--- cranangularclient/src/app/uicomps/itempager/itempager.component.spec.ts
+++ cranangularclient/src/app/uicomps/itempager/itempager.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {FormsModule} from '@angular/forms';
+import { Component, Input, DebugElement, TemplateRef} from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 
+import { CRAN_SERVICE_TOKEN } from '../../cran-data.servicetoken';
+import {NotificationService} from '../../notification.service';
+import {ConfirmService} from '../../confirm.service';
+import {LanguageService} from '../../language.service';
+import {PagedResult} from '../../model/pagedresult';
 import { ItempagerComponent } from './itempager.component';
+import {TooltipDirective} from '../tooltip.directive';
 
-describe('ItempagerComponent', () => {
+describe('PagerComponent', () => {
   let component: ItempagerComponent;
   let fixture: ComponentFixture<ItempagerComponent>;
 
   beforeEach(async(() => {
+    const cranDataService = jasmine.createSpyObj('CranDataService', ['vote']);
+    const notificationService = jasmine.createSpyObj('NotificationService', ['emitLoading', 'emitDone', 'emitError']);
+    const confirmationService = jasmine.createSpyObj('ConfirmService', ['some']);
+
     TestBed.configureTestingModule({
-      declarations: [ ItempagerComponent ]
+      imports: [RouterTestingModule, FormsModule],
+      declarations: [ ItempagerComponent, TooltipDirective ],
+      providers: [
+        LanguageService,
+        { provide: CRAN_SERVICE_TOKEN, useValue: cranDataService },
+        { provide: NotificationService, useValue: notificationService },
+        { provide: ConfirmService, useValue: confirmationService },
+      ],
     })
     .compileComponents();
   }));
@@ -19,7 +39,7 @@ describe('ItempagerComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should be created', () => {
     expect(component).toBeTruthy();
   });
 });
